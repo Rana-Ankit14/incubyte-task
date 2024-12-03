dcf086338f8dde74195a9d46a0652565b990e2d9
Rana Ankit <ranaasbwbs@gmail.com>
2024-12-03 13:15:13 +0530

    task: update add function in utils to handle sum

--- app/utils/add.ts
+++ app/utils/add.ts
@@ -1,5 +1,9 @@
 import "server-only";
 
 export const add = (numbers: string): number => {
-  return 0;
-}
+  const numberArr = numbers.split(",");
+  const sum = numberArr.reduce((prevValue, currentValue) => {
+    return prevValue + parseInt(currentValue);
+  }, 0);
+  return sum;
+};
