972d032e2cf01c8f9a062ac6b16666192edbc70b
Rana Ankit <ranaasbwbs@gmail.com>
2024-12-03 13:21:34 +0530

    updated add function to handle empty string, and update test cases for add to handle empty string

--- app/utils/add.test.ts
+++ app/utils/add.test.ts
@@ -3,6 +3,7 @@ import { add } from "./add";
 describe("String Calculator", () => {
   it("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
+    expect(add("     ")).toBe(0);
   });
 
   it("should return the sum of one number", () => {
--- app/utils/add.ts
+++ app/utils/add.ts
@@ -1,6 +1,10 @@
 import "server-only";
 
 export const add = (numbers: string): number => {
+  // checking for empty string
+  if (numbers.trim().length === 0) {
+    return 0;
+  }
   const numberArr = numbers.split(",");
   const sum = numberArr.reduce((prevValue, currentValue) => {
     return prevValue + parseInt(currentValue);
