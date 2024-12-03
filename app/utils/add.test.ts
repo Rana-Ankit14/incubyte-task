a8881d037a748cff6eb078b812906378106e7d4b
Rana Ankit <ranaasbwbs@gmail.com>
2024-12-03 13:04:54 +0530

    test: update test cases for add function

--- app/utils/add.test.ts
+++ app/utils/add.test.ts
@@ -1,5 +1,17 @@
 import { add } from "./add";
 
-test("Test functions that import server-only", () => {
-  expect(add(1, 2)).toBe(3);
+describe("String Calculator", () => {
+  it("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
+
+  it("should return the sum of one number", () => {
+    expect(add("1")).toBe(1);
+    expect(add("6")).toBe(6);
+  });
+
+  it("should return the sum of two number", () => {
+    expect(add("1,5")).toBe(6);
+    expect(add("5,9")).toBe(14);
+  });
 });
