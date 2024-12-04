23b5865e3f3390b8f3ec483e694f9a183f477933
Rana Ankit <ranaasbwbs@gmail.com>
2024-12-04 13:53:20 +0530

    update css styling for string calculator

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,12 @@ export default function HomePage() {
   const [value, setValue] = useState("");
   const [total, setTotal] = useState("");
   const [error, setError] = useState("");
+
   const handleCalculate = () => {
     try {
       const sum = add(value);
       setTotal(`${sum}`);
+      setError("");
     } catch (e) {
       setTotal("");
       setError((e as Error).message);
@@ -20,17 +22,19 @@ export default function HomePage() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.target.value);
   };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>String Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>String Calculator</h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>String Calculator</h1>
         <div>
-          <label htmlFor="calc">Enter String to be calculated</label>
-
+          <label htmlFor="calc" className={styles.label}>
+            Enter String to be calculated
+          </label>
           <input
             onChange={handleChange}
             type="text"
@@ -38,16 +42,17 @@ export default function HomePage() {
             name="calc"
             required
             placeholder="Enter your string to be calculated"
+            className={styles.input}
           />
         </div>
         <div>
-          <button onClick={handleCalculate}>Calculate</button>
+          <button onClick={handleCalculate} className={styles.button}>
+            Calculate
+          </button>
         </div>
         <div>
-          <h4>Total Sum : {total}</h4>
-        </div>
-        <div>
-          <h4>{error}</h4>
+          <h4 className={styles.total}>Total Sum : {total}</h4>
+          {error && <h4 className={styles.error}>{error}</h4>}
         </div>
       </main>
     </div>
