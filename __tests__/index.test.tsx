13fbbb9e11a11d4d5a36051a263cbeaff051f986
Rana Ankit <ranaasbwbs@gmail.com>
2024-12-05 16:18:07 +0530

    update test for the main home page

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,17 +1,24 @@
-/**
- * @jest-environment jsdom
- */
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/index";
+import HomePage from "@/pages/index";
 
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+describe("HomePage Component", () => {
+  it("renders the calculator panel in the left panel and user instructions in the right panel", () => {
+    render(<HomePage />);
 
-    const heading = screen.getByRole("heading", {
-      name: /welcome to next\.js!/i,
+    const mainHeading = screen.getByRole("heading", {
+      name: /string calculator/i,
+      level: 1,
     });
+    expect(mainHeading).toBeInTheDocument();
+    expect(
+      screen.getByLabelText(/enter string to be calculated/i)
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: /calculate/i })
+    ).toBeInTheDocument();
 
-    expect(heading).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: /how to use the string calculator/i })
+    ).toBeInTheDocument();
   });
 });
